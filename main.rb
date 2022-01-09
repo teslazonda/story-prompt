5eb0d8c53ba8b3f9f7c53815fa91a3997ce73229
Quill Cronwall <quilljazz@gmail.com>
2022-01-08 16:39:04 -0800

    initial commit

---
+++ main.rb
@@ -0,0 +1,42 @@
+require 'json'
+# One day Anna was walking her {{NUMBER}} {{UNIT_OF_MEASURE}} commute to {{PLACE}} and found a {{ADJECTIVE}} {{NOUN}} on the ground.
+
+# Write a command line application in any language that accepts a json string of key-value inputs for the template above.
+# With valid input, the application sends to STDOUT the story using the inputs provided. For example, "One day Anna was walking her 2 mile commute to school and found a blue rock on the ground." The application stores a record of valid inputs locally in a file. For the template above, you can assume NUMBER to be numerical data and all other inputs to be strings. Set sensible string validations for length.
+
+# Write a second command line application that sends to STDOUT statistics about the stored records, including the maximum and minimum values for numerical inputs, the most common responses for string inputs, and anything else you think might be relevant.
+
+# Method for json validation
+def valid_json?(string)
+  !!JSON.parse(string)
+rescue JSON::ParserError
+  false
+end
+sample_json = {
+  'NUMBER' => 1,
+  'UNIT_OF_MEASURE' => 'meter',
+  'PLACE' => 'Reillyville',
+  'ADJECTIVE' => 'red',
+  'NOUN' => 'hat'
+}
+
+# generate a JSON test string
+json_data = JSON.generate(sample_json)
+
+puts 'Welcome to the CommonPaper story prompt generator! Please enter a valid JSON to construct your prompt, or copy and paste the sample JSON below'
+puts "\n"
+puts json_data
+puts "\n"
+input = gets.chomp
+# Validate JSON input
+if input.length < 300 && valid_json?(input)
+  parsed_input = JSON.parse(input)
+  message = "One day Anna was walking her #{parsed_input['NUMBER']} #{parsed_input['UNIT_OF_MEASURE']} commute to #{parsed_input['PLACE']} and found a #{parsed_input['ADJECTIVE']} #{parsed_input['NOUN']} on the ground."
+  puts "\n"
+  puts '***Prompt created***'
+  puts message
+  File.open('out.txt', 'a+') { |f| f.puts(message) }
+
+else
+  puts '----- Error invalid JSON input please start again with a valid JSON'
+end
---
+++ stats.rb
@@ -0,0 +1,43 @@
+if File.exist?('out.txt')
+  outputs_array = []
+  number_array = []
+  unit_of_measure_array = []
+  place_array = []
+  adjective_array = []
+  noun_array = []
+  File.readlines('out.txt').each do |line|
+    line_array = line.split(' ')
+    outputs_array << line_array
+  end
+  # Creates an array of arrays, where each inner array is a line in out.txt
+  outputs_array.each do |line|
+    number_array << line[6].to_i
+    unit_of_measure_array << line[7]
+    place_array << line[10]
+    adjective_array << line[14]
+    noun_array << line[15]
+  end
+  # Create frequency tables for each array and show max in console
+  max_numberical_input = number_array.max
+  min_numerical_input = number_array.min
+  # nouns
+  noun_freq = noun_array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  noun_max = noun_array.max_by { |v| noun_freq[v] }
+  # adjectives
+  adjective_freq = adjective_array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  adjective_max = adjective_array.max_by { |v| adjective_freq[v] }
+  # places
+  place_freq = place_array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  place_max = place_array.max_by { |v| place_freq[v] }
+  # unit_of_measures
+  unit_of_measure_freq = unit_of_measure_array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  unit_of_measure_max = unit_of_measure_array.max_by { |v| unit_of_measure_freq[v] }
+  # Print stats in the terminal
+  puts '--- INPUT STATISTICS ---'
+  puts "Maximum Numerical input: #{max_numberical_input}"
+  puts "Minimum Numerical input: #{min_numerical_input}"
+  puts "Most common Unit-of-measure response: '#{unit_of_measure_max}', occuring #{unit_of_measure_freq[unit_of_measure_max]} times"
+  puts "Most common Place response: '#{place_max}', occuring #{place_freq[place_max]} times"
+  puts "Most common Adjective response: '#{adjective_max}', occuring #{adjective_freq[adjective_max]} times"
+  puts "Most common Noun response: '#{noun_max}', occuring #{noun_freq[noun_max]} times"
+end
